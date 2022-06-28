01bd453b3de4b66d62f7bf1cd84d8481b07f7952
iolq <wipawee_watt@cmu.ac.th>
2022-06-28 09:58:01 +0700

    committed 28/06

---
+++ src/component/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'เทคโนโลยี',
+        path: '/tech-course',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'การเป็นผู้ประกอบการ',
+        path: '/enterp-course',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'การเงินการลงทุน',
+        path: '/finance-course',
+        cName: 'dropdown-link'
+    }
+];
